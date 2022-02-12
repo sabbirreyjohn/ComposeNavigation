94e7daab39aa223874337cf838f524e4f8d51d17
Md Sabbir Ahmed Khan <sabbir.ahmed.khan.reyjohn@gmail.com>
2022-02-12 17:22:26 +0600

    Status sealed class integrated for better event handling

--- app/src/main/java/com/androidrey/composenavigation/ui/view/userlist/UserListViewModel.kt
+++ app/src/main/java/com/androidrey/composenavigation/ui/view/userlist/UserListViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.androidrey.composenavigation.datasource.getDatabase
 import com.androidrey.composenavigation.model.User
 import com.androidrey.composenavigation.repository.DataRepository
+import com.androidrey.composenavigation.util.Status
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import java.io.IOException
@@ -17,18 +18,44 @@ class UserListViewModel(application: Application) : AndroidViewModel(application
     private val _users = MutableStateFlow<List<User>>(mutableListOf())
     val users get() = _users
 
+    private val _isLoading = MutableStateFlow<Boolean>(false)
+    val isLoading get() = _isLoading
+
+    private val _hasError = MutableStateFlow<Boolean>(false)
+    val hasError get() = _hasError
+
     init {
         loadUserData()
     }
 
     fun loadUserData() {
         viewModelScope.launch {
-            try {
-                val tempUsers = repo.getUsersFromServer()
-                repo.insertUsersToDB(tempUsers)
-                _users.emit(repo.getUsersFromDB())
-            } catch (exception: IOException) {
-                _users.emit(repo.getUsersFromDB())
+            _isLoading.value = true
+            val status = repo.getUsersFromServer()
+            when (status) {
+                is Status.Success -> {
+                    repo.insertUsersToDB(status.data!!)
+                    loadUserListFromDB()
+                }
+                is Status.Error -> {
+                    loadUserListFromDB()
+                }
+
+            }
+        }
+    }
+
+    private suspend fun loadUserListFromDB() {
+        val dbStatus = repo.getUsersFromDB()
+        _isLoading.value = false
+        when (dbStatus) {
+            is Status.Success -> {
+                _hasError.value = false
+                _users.value = dbStatus.data!!
+            }
+            is Status.Error -> {
+                _hasError.value = true
+                _users.value = mutableListOf()
             }
         }
     }
